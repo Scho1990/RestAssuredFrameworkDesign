d505d8f2395da9852f1a32a585edbf0cedda62db
Scho1990 <it.santosh68@gmail.com>
2024-01-28 14:53:27 +0530

    third Commit for rest assured

--- src/test/java/com/rest/RequestAndResponseSpecBuilderPattern.java
+++ src/test/java/com/rest/RequestAndResponseSpecBuilderPattern.java
@@ -1,45 +1,48 @@
 package com.rest;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import static io.restassured.RestAssured.with;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 import static io.restassured.RestAssured.*;
 
-public class RequestSpecBuilderPattern {
+public class RequestAndResponseSpecBuilderPattern {
 
     RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
 
     @BeforeClass
     public void beforeClass(){
-       /* requestSpecification= with().
-                baseUri("https://api.postman.com").
-                header("X-Api-Key","PMAK-65b37a978b91a6000175b61b-f94a81a0deb7fb46e0fd09aee0063d2b41").
-                log().all();*/
         RequestSpecBuilder requestSpecBuilder=new RequestSpecBuilder();
         requestSpecBuilder.setBaseUri("https://api.postman.com");
         requestSpecBuilder.addHeader("X-Api-Key","PMAK-65b37a978b91a6000175b61b-f94a81a0deb7fb46e0fd09aee0063d2b41");
         requestSpecBuilder.log(LogDetail.ALL);
         requestSpecification=requestSpecBuilder.build();
+
+        ResponseSpecBuilder responseSpecBuilder=new ResponseSpecBuilder();
+        responseSpecBuilder.expectStatusCode(200).
+        expectContentType(ContentType.JSON).
+        log(LogDetail.ALL);
+        responseSpecification=responseSpecBuilder.build();
     }
 
     @Test
     public void validate_get_status_code(){
-        Response response = given(requestSpecification).get("/workspaces").then().log().all().extract().response();
-        assertThat(response.statusCode(),is(equalTo(200)));
+        given(requestSpecification).get("/workspaces").
+                then().spec(responseSpecification).extract().response();
     }
 
     @Test
     public void validate_get_response_body(){
-        Response response =given(requestSpecification).get("/workspaces").then().log().all().extract().response();
-        assertThat(response.statusCode(),is(equalTo(200)));
+        Response response =given(requestSpecification).get("/workspaces").
+                then().spec(responseSpecification).extract().response();
         assertThat(response.path("workspaces[0].name"),equalTo("My Workspace"));
     }
 }
--- src/test/java/com/rest/RequestAndResponseSpecificationExample.java
+++ src/test/java/com/rest/RequestAndResponseSpecificationExample.java
@@ -1,20 +1,25 @@
 package com.rest;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.equalTo;
 
-public class RequestSpecificationExample {
+public class RequestAndResponseSpecificationExample {
     RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
 
     @BeforeClass
     public void beforeClass(){
          requestSpecification= given().
                 baseUri("https://api.postman.com").
                 header("X-Api-Key","PMAK-65b37a978b91a6000175b61b-f94a81a0deb7fb46e0fd09aee0063d2b41");
+         responseSpecification= RestAssured.expect().statusCode(200).contentType(ContentType.JSON);
     }
 
     @Test
@@ -23,9 +28,7 @@ public class RequestSpecificationExample {
         when().
                 get("/workspaces").
         then().
-                log().all().
-                assertThat().
-                statusCode(200);
+                spec(responseSpecification);
     }
 
     @Test
@@ -34,9 +37,7 @@ public class RequestSpecificationExample {
                 when().
                 get("/workspaces").
                 then().
-                log().all().
-                assertThat().
-                statusCode(200).
+                spec(responseSpecification).
                 body("workspaces.name",hasItems("My Workspace","AddActiveDirectory"),
                         "workspaces[0].name",equalTo("My Workspace"),
                         "workspaces[0].name",is(equalTo("My Workspace")),
